accb7930b84b64a9b36c017eb6a520ec062959ea
jFujiya <shiho.fujiya@anti-pattern.co.jp>
2024-03-29 08:47:08 +0900

    15week215week215week215week215week215week215week215week215week215week215week215week215week215week215week2

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/dummy-data', function () {
     $data = [
         [
-            'name' => 'John Doe',
+            'name' => 'Michael Andrew Fox',
             'email' => 'john.doe@example.com',
             'age' => 30,
         ],
